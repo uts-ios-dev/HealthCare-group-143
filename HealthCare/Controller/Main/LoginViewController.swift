4d63707bd0b2f72168c3a358efb821bc79861df3
darrenhk2010 <darrenhk2010@outlook.com>
2019-05-23 04:04:49 +1000

    Update User part
    1. Able to switch view
    2. reset password
    3. update password
    4. update profile

--- HealthCare/Controller/Main/LoginViewController.swift
+++ HealthCare/Controller/Main/LoginViewController.swift
@@ -17,9 +17,10 @@ class LoginViewController: UIViewController, UITextFieldDelegate{
     //Move TextField when typing
     var activeTextField: UITextField!
     
-    var roleReference = Database.database().reference().child("User")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         changeLayout()
         self.emailTextField.delegate = self
         self.passwordTextField.delegate = self
@@ -31,6 +32,44 @@ class LoginViewController: UIViewController, UITextFieldDelegate{
         self.hideKeyboard()
         
     }
+
+    override func viewWillLayoutSubviews() {
+        checkUser()
+    }
+    
+    @objc func checkUser(){
+        if Auth.auth().currentUser?.uid != nil{
+            let role = UserDefaults.standard.string(forKey: "role")
+            if(role == "doctor"){
+                let vc = UIStoryboard(name:"Main", bundle:nil).instantiateViewController(withIdentifier: "DoctorView")
+                let viewController = self
+                // Present the view controller
+                let currentViewController = UIApplication.shared.keyWindow?.rootViewController
+                currentViewController?.dismiss(animated: true, completion: nil)
+                
+                if viewController.presentedViewController == nil {
+                    currentViewController?.present(vc, animated: false, completion: nil)
+                } else {
+                    viewController.present(vc, animated: false, completion: nil)
+                }
+            }
+            else{
+                let vc = UIStoryboard(name:"Main", bundle:nil).instantiateViewController(withIdentifier: "homeViewController")
+                let viewController = self
+                // Present the view controller
+                let currentViewController = UIApplication.shared.keyWindow?.rootViewController
+                currentViewController?.dismiss(animated: true, completion: nil)
+                
+                if viewController.presentedViewController == nil {
+                    currentViewController?.present(vc, animated: true, completion: nil)
+                } else {
+                    viewController.present(vc, animated: true, completion: nil)
+                }
+            }
+
+        }
+    }
+    
     @objc func keyboardDidShow(notification: Notification){
         let info:NSDictionary = notification.userInfo! as NSDictionary
         let keyboardSize = (info[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
@@ -75,14 +114,17 @@ class LoginViewController: UIViewController, UITextFieldDelegate{
         
         
         Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            let roleReference = Database.database().reference().child("User")
             if user != nil{
                 let userID = Auth.auth().currentUser?.uid
-                self.roleReference.child(userID!).child("role").observeSingleEvent(of: .value, with: { (snapshot) in
+                roleReference.child(userID!).child("role").observeSingleEvent(of: .value, with: { (snapshot) in
                     if let accountType = snapshot.value as? String{
                         if accountType == "user"{
+                            UserDefaults.standard.set("user", forKey: "role")
                             self.performSegue(withIdentifier: "loginHome", sender: self)
                         }
                         if accountType == "doctor"{
+                            UserDefaults.standard.set("doctor", forKey: "role")
                             self.performSegue(withIdentifier: "doctorHomeView", sender: self)
                         }
                     }
@@ -115,7 +157,43 @@ class LoginViewController: UIViewController, UITextFieldDelegate{
     
     
     @IBAction func resetPassword(_ sender: UIButton) {
-        performSegue(withIdentifier: "resetPassword", sender: self)
+        var title = "Forgot Password"
+        var message = "Do you want to reset your password?"
+        let email = emailTextField.text
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        
+        
+        if(email != ""){
+            self.present(alert,animated: true, completion: nil)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .default) { (action) in
+            })
+            alert.addAction(UIAlertAction(title: "Yes", style: .default) { (action) in
+                Auth.auth().sendPasswordReset(withEmail: email!) { error in
+                    title = "Successfully!"
+                    message = "Please check your inbox!"
+                    let alert2 = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                    alert2.addAction(UIAlertAction(title: "Dismiss", style: .default))
+                    self.present(alert2,animated: true, completion: nil)
+                    if(error != nil){
+                        message = "Error was happen \(error)"
+                        let alert2 = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                        alert2.addAction(UIAlertAction(title: "Dismiss", style: .default))
+                        self.present(alert2,animated: true, completion: nil)
+                    }
+                }
+            })
+        }
+        else{
+            message = "Please enter your email address"
+            let alert2 = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alert2.addAction(UIAlertAction(title: "Dismiss", style: .default))
+            self.present(alert2,animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func SignupView(_ sender: UIButton) {
+        performSegue(withIdentifier: "SignupView", sender: self)
     }
     
 }
